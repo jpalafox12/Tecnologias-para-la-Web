f39163b6f08d9838e37ddeb30833024620ad7da0
Jesus Palafox <Jesus Palafox@DESKTOP-2THS7IL>
2021-04-21 13:01:03 -0500

    ejercicio5_JPS

---
+++ departamental2/js/calculadora.js
@@ -0,0 +1,17 @@
+
+function sumaNumeros() {
+    alert( document.getElementById("numero1").value );
+    alert( document.getElementById("numero2").value );
+}
+
+function restaNumeros() {
+    alert("Para restar");
+}
+
+function multiplicaNumeros() {
+    alert("Para multiplicar");
+}
+
+function divideNumeros() {
+    alert("Para dividir");
+}
