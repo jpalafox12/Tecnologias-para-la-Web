42e0e9e6355c49ffe186cc530a64db4ce25d0eb2
Jesus Palafox <Jesus Palafox@DESKTOP-2THS7IL>
2021-04-22 15:22:09 -0500

    ...

--- departamental2/js/calculadoraCientifica.js
+++ departamental2/js/calculadoraCientifica.js
@@ -1,34 +1,12 @@
+function presionaDigito(digito){
+	var display 		= document.getElementById("display");
+	display.value		= display.value + digito;
 
-function sumaNumeros() {
-	//Acceder a los elementos de mi documento
-	var num1 		= document.getElementById("numero1").value; //String
-	var num2 		= document.getElementById("numero2").value;	//String
-	var resultado 	= document.getElementById("resultado");
-	
-	//Proceso
-	var suma = parseInt(num1) + parseInt(num2); //Suma
+	//alert("Diste click en el digito: "+digito);
 
-	//Mostrar
-	resultado.innerHTML="Resultado: " + suma;
 }
 
-function restaNumeros() {
-	var resta = document.getElementById("numero1").value - document.getElementById("numero2").value;
-	document.getElementById("resultado").innerHTML ="Resultado: " + resta;
-}
-
-function multiplicaNumeros() {
-    var num1 = document.getElementById("numero1").value;
-    var num2 = document.getElementById("numero2").value;
-    var resultado = document.getElementById("resultado");
-    var multiplicacion = num1 * num2;
-    resultado.innerHTML = "Resultado: " + multiplicacion;
-}
-
-function divideNumeros() {
-   var num1 = document.getElementById("numero1").value;
-    var num2 = document.getElementById("numero2").value;
-    var resultado = document.getElementById("resultado");
-    var division = num1 / num2;
-    resultado.innerHTML = "Resultado: " + division;
+function limpiaDisplay(){
+	var display = document.getElementById("display");
+	display.value="0";
 }
